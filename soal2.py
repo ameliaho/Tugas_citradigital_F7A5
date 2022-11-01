ec1c58c23fe58c5e6595c3a494ef3eadd99ea4d8
ameliiaho <85068490+ameliiaho@users.noreply.github.com>
2022-11-01 13:58:15 +0700

    Add files via upload

---
+++ soal2.py
@@ -0,0 +1,7 @@
+print("Bilangan terurut")
+i = 0;
+x = int(input("Masukkan bilangan = "))
+
+while i < 18:
+    i=i+3;
+    print(i)
---
+++ soal3.py
@@ -0,0 +1,7 @@
+print("Bilangan terurut")
+i = 19;
+x = int(input("Masukkan bilangan = "))
+
+while i > 3:
+    i=i-2;
+    print(i)
